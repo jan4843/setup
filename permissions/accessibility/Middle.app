4f0f63a5d1c3f4ea1b1b029b8424154e7d3a4bcd
Jan Vitturi <vitturi.jan@gmail.com>
2022-12-08 16:01:04 +0100

    Add Middle

---
+++ permissions/accessibility/Middle.app
@@ -0,0 +1,1 @@
+/Applications/Middle.app
---
+++ settings/apps/middle.sh
@@ -0,0 +1,4 @@
+defaults write com.knollsoft.Middle mouseTwoClick -bool true
+defaults write com.knollsoft.Middle trackpadFourTap -bool true
+
+defaults write com.knollsoft.Middle hideMenuBarIcon -bool true
