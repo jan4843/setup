be36cf84b0e3700b40565fe372cae746a0ec9314
Jan Vitturi <vitturi.jan@gmail.com>
2022-12-17 16:52:52 +0100

    Add Numbers

--- brew/personal.rb
+++ brew/personal.rb
@@ -7,6 +7,7 @@ cask "telegram"
 cask "whatsapp"
 
 mas "AdGuard for Safari", id: 1440147259
+mas "Numbers", id: 409203825
 mas "Spark", id: 1176895641
 mas "SponsorBlock for YouTube - Skip Sponsorships", id: 1573461917
 mas "Super Agent", id: 1568262835
