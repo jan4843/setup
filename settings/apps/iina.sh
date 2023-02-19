e4b1fa142891cf435c9193fa845f516e37be2093
Jan Vitturi <vitturi.jan@gmail.com>
2023-02-19 19:42:41 +0100

    Add IINA settings

---
+++ settings/apps/iina.sh
@@ -0,0 +1,3 @@
+defaults write com.colliderli.iina actionAfterLaunch -int 2
+defaults write com.colliderli.iina verticalScrollAction -int 2
+defaults write com.colliderli.iina horizontalScrollAction -int 2
