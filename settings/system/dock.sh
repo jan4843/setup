6b08f115586c023563f2326b20f0c86e26674a2d
Jan Vitturi <vitturi.jan@gmail.com>
2023-02-20 23:25:40 +0100

    Add Dock persistent apps

--- settings/system/dock.sh
+++ settings/system/dock.sh
@@ -5,4 +5,23 @@ defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -int 0
 defaults write com.apple.dock autohide-time-modifier -float 0.5
 
-defaults write com.apple.dock persistent-apps -array ''
+defaults write com.apple.dock show-recent-count -int 1
+
+defaults write com.apple.dock persistent-apps -array
+for app_dir in /Applications/Mimestream.app; do
+    [ -e "$app_dir" ] || continue
+    defaults write com.apple.dock persistent-apps -array-add "
+    <dict>
+    <key>tile-data</key>
+    <dict>
+        <key>file-data</key>
+        <dict>
+        <key>_CFURLString</key>
+        <string>$app_dir</string>
+        <key>_CFURLStringType</key>
+        <integer>0</integer>
+        </dict>
+    </dict>
+    </dict>
+    "
+done
