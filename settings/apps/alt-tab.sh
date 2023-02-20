eefa1782fd013d508df9e14e23bf7379f3236a2d
Jan Vitturi <vitturi.jan@gmail.com>
2023-02-20 23:29:27 +0100

    Revert "Add AltTab"
    This reverts commit 103792d6b24b100e473d948b364118187ae2a2ba.

--- settings/apps/alt-tab.sh
+++
@@ -1,4 +0,0 @@
-defaults write com.lwouis.alt-tab-macos titleTruncation -string "1"
-defaults write com.lwouis.alt-tab-macos hideSpaceNumberLabels -string "true"
-defaults write com.lwouis.alt-tab-macos hideColoredCircles -string "true"
-defaults write com.lwouis.alt-tab-macos hideWindowlessApps -string "true"
