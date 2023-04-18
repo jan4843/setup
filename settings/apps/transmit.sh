1c7bf7a3821cb837bc4f753a64b6e138f116dca6
Jan Vitturi <vitturi.jan@gmail.com>
2023-04-18 11:39:53 +0200

    Sync entire Transmit config

--- settings/apps/transmit.sh
+++ settings/apps/transmit.sh
@@ -2,6 +2,4 @@ defaults write com.panic.transmit SplitCollapsedIndex -int 0
 
 defaults write com.panic.transmit ShowHiddenFiles -bool true
 
-mkdir -p ~/Library/Application\ Support/Transmit
-ln -sf ~/Library/Dropbox/Transmit/Connections.transmitstore ~/Library/Application\ Support/Transmit
-ln -sf ~/Library/Dropbox/Transmit/Metadata ~/Library/Application\ Support/Transmit
+ln -sf ~/Library/Dropbox/Transmit ~/Library/Application\ Support
