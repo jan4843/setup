305d2816f369c03f199c9ded3e33c6ecb8f8ee98
Jan Vitturi <vitturi.jan@gmail.com>
2023-04-18 12:28:40 +0200

    Add Infuse

--- brew/personal.rb
+++ brew/personal.rb
@@ -7,6 +7,7 @@ cask "netnewswire"
 cask "telegram"
 cask "whatsapp"
 mas "AdGuard for Safari", id: 1440147259
+mas "Infuse", id: 1136220934
 mas "Numbers", id: 409203825
 mas "Spark", id: 1176895641
 mas "SponsorBlock for YouTube - Skip Sponsorships", id: 1573461917
