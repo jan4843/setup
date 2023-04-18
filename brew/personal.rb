b9d8498e04c70e6323bde12e0c4445daaa58e7c6
Jan Vitturi <vitturi.jan@gmail.com>
2023-04-18 12:28:16 +0200

    Update Userscripts Brewfile name

--- brew/personal.rb
+++ brew/personal.rb
@@ -11,4 +11,4 @@ mas "Numbers", id: 409203825
 mas "Spark", id: 1176895641
 mas "SponsorBlock for YouTube - Skip Sponsorships", id: 1573461917
 mas "Super Agent", id: 1568262835
-mas "Userscripts", id: 1463298887
+mas "Userscripts-Mac-App", id: 1463298887
