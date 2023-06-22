aefd484b1ba4cfd8805a7c2e2f0b3134c0454e51
Jan Vitturi <vitturi.jan@gmail.com>
2023-06-22 23:57:38 +0200

    Fix mouse settings

--- settings/system/mouse.sh
+++ settings/system/mouse.sh
@@ -1,3 +1,3 @@
 defaults write com.apple.AppleMultitouchMouse MouseButtonMode -string TwoButton
-defaults write com.apple.AppleMultitouchMouse MouseOneFingerDoubleTapGesture -bool true
+defaults write com.apple.AppleMultitouchMouse MouseOneFingerDoubleTapGesture -int 1
 defaults write -g AppleEnableMouseSwipeNavigateWithScrolls -bool true
