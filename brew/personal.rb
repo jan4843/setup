5fa9acf70e3170fe3c47e7d14fe4b9baca0854d6
Jan Vitturi <vitturi.jan@gmail.com>
2023-06-23 00:12:03 +0200

    Replace AdGuard and Super Agent with Ghostery

--- brew/personal.rb
+++ brew/personal.rb
@@ -6,10 +6,9 @@ cask "netnewswire"
 cask "sonos"
 cask "telegram"
 cask "whatsapp"
-mas "AdGuard for Safari", id: 1440147259
+mas "Ghostery – Privacy Ad Blocker", id: 1436953057
 mas "Infuse", id: 1136220934
 mas "Numbers", id: 409203825
 mas "Spark", id: 1176895641
 mas "SponsorBlock for YouTube - Skip Sponsorships", id: 1573461917
-mas "Super Agent", id: 1568262835
 mas "Userscripts-Mac-App", id: 1463298887
