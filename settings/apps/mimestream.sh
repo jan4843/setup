2ea96f28a902b5cdcc065ac81c4eaabb3661442d
Jan Vitturi <vitturi.jan@gmail.com>
2023-06-22 23:56:43 +0200

    Update Mimestream settings

--- settings/apps/mimestream.sh
+++ settings/apps/mimestream.sh
@@ -1,5 +1,9 @@
+defaults write ~/Library/Containers/com.mimestream.Mimestream/Data/Library/Preferences/com.mimestream.Mimestream.plist LeadingEdgeSwipeAction -string archive
+defaults write ~/Library/Containers/com.mimestream.Mimestream/Data/Library/Preferences/com.mimestream.Mimestream.plist TrailingEdgeSwipeAction -string trash
 defaults write ~/Library/Containers/com.mimestream.Mimestream/Data/Library/Preferences/com.mimestream.Mimestream.plist ListStyle -string "expanded"
 defaults write ~/Library/Containers/com.mimestream.Mimestream/Data/Library/Preferences/com.mimestream.Mimestream.plist HideBadgeForSpam -bool true
 defaults write ~/Library/Containers/com.mimestream.Mimestream/Data/Library/Preferences/com.mimestream.Mimestream.plist HideMoreSection -bool true
 
 defaults write ~/Library/Containers/com.mimestream.Mimestream/Data/Library/Preferences/com.mimestream.Mimestream.plist BlockRemoteImages -bool true
+
+defaults write ~/Library/Containers/com.mimestream.Mimestream/Data/Library/Preferences/com.mimestream.Mimestream.plist ShowEntireAddressWhenComposing -bool true
