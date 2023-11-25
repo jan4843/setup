6e81e1be6e09f19eccc9d8443c7475dd16ee0b77
Jan Vitturi <vitturi.jan@gmail.com>
2023-11-25 22:56:52 +0100

    Add Steam Link

--- profiles/personal.rb
+++ profiles/personal.rb
@@ -23,4 +23,5 @@ mas "Infuse", id: 1136220934
 mas "Numbers", id: 409203825
 mas "Spark", id: 1176895641
 mas "SponsorBlock for YouTube - Skip Sponsorships", id: 1573461917
+mas "Steam Link", id: 1246969117
 mas "Userscripts-Mac-App", id: 1463298887
